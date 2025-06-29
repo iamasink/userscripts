4c24a4bc109ec82e9d98ee35d7dd656c30945524
iamasink <iamasink@proton.me>
2025-06-29 19:10:06 +0100

    add music button & scroll volume scripts

--- README.md
+++ README.md
@@ -2,11 +2,26 @@
 Various userscripts I've made.  
 These were made for [Violentmonkey](https://violentmonkey.github.io/) so might not work on other userscript-y extensions
 
+# YouTube
 ## YouTubeNewTab.user.js
 > Always open youtube homepage links in new tab. The homepage takes forever to (properly) load, so just leave it open!  
 
 [Install](YouTubeNewTab.user.js?raw=1)
 
+## YouTubeMixToMusicButton.user.js
+> Add a button to open current YouTube Mix in YouTube Music  
+
+[Install](YouTubeMixToMusicButton.user.js?raw=1)
+
+
+# YouTube Music
+## YouTubeMusicScrollVolume.user.js
+> Use scroll wheel to change volume anywhere on YouTube Music  
+
+[Install](YouTubeMusicScrollVolume.user.js?raw=1)  
+
+
+# Other
 ## SteamHideUser.user.js
 > Hide your Steam sign-in name  
 
---
+++ YouTubeMusicScrollVolume.user.js
@@ -0,0 +1,110 @@
+// ==UserScript==
+// @name        YouTube Music Scroll volume change
+// @namespace   Violentmonkey Scripts
+// @version     1.0
+// @description Use scroll wheel to change volume anywhere on YouTube Music
+// @match       https://music.youtube.com/*
+// @grant       none
+// @author      iamasink
+// @homepage    https://github.com/iamasink/userscripts
+// @supportURL  https://github.com/iamasink/userscripts/issues
+// @downloadURL https://raw.githubusercontent.com/iamasink/userscripts/main/YouTubeMusicScrollVolume.user.js
+// @updateURL   https://raw.githubusercontent.com/iamasink/userscripts/main/YouTubeMusicScrollVolume.user.js
+// @tag         youtube
+// @icon        https://www.google.com/s2/favicons?domain=youtube.com
+// @license     MIT
+// ==/UserScript==
+
+
+(function () {
+	/////// options ///////
+	const LOGGING_ENABLED = true
+
+
+
+	///////         ///////
+
+	// script full name from @name
+	const SCRIPT_NAME = GM_info.script.name
+	// script short name from updateurl filename
+	const SCRIPT_SHORTNAME = GM_info.script.updateURL.split("/").slice(-1)[0].split(".").slice(0, -2).join(".")
+	// script version
+	const SCRIPT_VERSION = GM_info.script.version
+
+	const logprefix = `[${SCRIPT_SHORTNAME}]`
+	function log(...args) {
+		if (!LOGGING_ENABLED) return
+		console.log(logprefix, ...args)
+	}
+	function logwarn(...args) {
+		console.warn(logprefix, ...args)
+	}
+	function logerror(...args) {
+		console.error(logprefix, ...args)
+	}
+
+	console.log(`[${SCRIPT_SHORTNAME}] ${SCRIPT_NAME} v${SCRIPT_VERSION} by iamasink loaded`)
+
+	let volumeSliderTimeout
+
+	addEventListener("wheel", (event) => {
+		// log(event)
+
+		// ignore ctrl+scroll zooms
+		if (event.ctrlKey) return
+
+		// ignore if over scrollable element
+		if (isInsideScrollable(event.target)) return
+
+		const player = document.querySelector('#movie_player')
+		if (!player) {
+			logwarn("no player. is the page still loading?")
+			return
+		}
+
+		const delta = Math.sign(-event.deltaY)
+		const step = 2
+
+		try {
+			const currentVol = player.getVolume()
+			const newVol = Math.min(100, Math.max(0, currentVol + (step * delta)))
+			if (player.isMuted() && newVol > 0) player.unMute()
+			player.setVolume(newVol)
+
+			// update volume slider
+			const volumeSlider = document.querySelector('tp-yt-paper-slider#volume-slider')
+			if (volumeSlider) {
+				volumeSlider.value = newVol
+				volumeSlider.classList.add("on-hover")
+
+				// remove hover after timeout (hide slider)
+				clearTimeout(volumeSliderTimeout)
+				volumeSliderTimeout = setTimeout(() => {
+					volumeSlider.classList.remove("on-hover")
+				}, 1000)
+			}
+
+			log(`changed volume: ${newVol}`)
+
+		} catch (e) {
+			logwarn("couldn't get/set volume. is the page still loading?", e)
+		}
+	}, { passive: true })
+
+	function isInsideScrollable(el) {
+		while (el && el !== document.body) {
+			const style = getComputedStyle(el)
+			const scrollY = el.scrollHeight > el.clientHeight
+			const scrollX = el.scrollWidth > el.clientWidth
+			const overflow = style.overflow + style.overflowY + style.overflowX
+			if ((scrollY || scrollX) && /auto|scroll/.test(overflow)) return true
+			// prevent on playlist page
+			if (el.id === "contents") return true
+			// prevent when hovering volume slider (own implementation)
+			if (el.id === "volume-slider") return true
+			el = el.parentElement
+		}
+		return false
+	}
+
+})()
