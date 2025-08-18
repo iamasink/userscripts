cd6a3b3473cd5afe2e1b79f2ef8b418c732d3a85
iamasink <iamasink@proton.me>
2025-08-18 22:58:14 +0100

    update

--- src/userscript/YouTubeScrollGestures.ts
+++ src/userscript/YouTubeScrollGestures.ts
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        YouTube Scroll Gestures
 // @namespace   Violentmonkey Scripts
-// @version     1.9
+// @version     1.10
 // @description Adds scroll gestures for Speed (ctrl+scroll) and Volume (rclick+scroll) like from "Enhancer for YouTube™"
 // @match       https://www.youtube.com/*
 // @grant       GM_getValue
